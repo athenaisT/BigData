0c02251239cd2d1f361a78fb91a14c630c64b65f
athenaisT <athenais.thiburce@gmail.com>
2022-03-15 16:59:32 +0100

    Menue python main

---
+++ main.py
@@ -0,0 +1,35 @@
+
+menu_options = {
+    1: 'Images',
+    2: 'Stats user',
+    4: 'Exit',
+}
+
+def print_menu():
+    for key in menu_options.keys():
+        print (key, '--', menu_options[key] )
+
+def option1():
+     print('Handle option \'Option 1\'')
+
+def option2():
+     print('Handle option \'Option 2\'')
+
+if __name__=='__main__':
+    while(True):
+        print_menu()
+        option = ''
+        try:
+            option = int(input('Enter your choice: '))
+        except:
+            print('Wrong input. Please enter a number ...')
+        #Check what choice was entered and act accordingly
+        if option == 1:
+           option1()
+        elif option == 2:
+            option2()
+        elif option == 4:
+            print('Au revoir')
+            exit()
+        else:
+            print('Mauvaise saisie.')
